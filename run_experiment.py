e5061bf0d8e3e7634aab62d756626ae765efb376
Austin Murphy <murphy.austink@gmail.com>
2019-12-10 14:31:49 -0800

    run experiment and sim impute prep

--- run_experiment.py
+++ run_experiment.py
@@ -1,11 +1,25 @@
 import argparse
-
+from sim_impute import *
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+<<<<<<< HEAD
     parser.add_argument('--tau', type=float, default=0.0)
     args = parser.parse_args()
 
     args.tau
+=======
+    parser.add_argument('--tau', type=float, required=True)
+    parser.add_argument('--num_sim', type=int, default=10)
+    parser.add_argument('--distn', type=str, default="LogNormal")
+    parser.add_argument('--rho', type=float, default=0.0)
+    parser.add_argument('--obs_conf', type=bool, default=False)
+    args = parser.parse_args()
+    
+    p = run_sim(args.num_sim, eval(args.distn), args.tau, args.rho, args.obs_conf)
+    
+    np.savetxt('{}.out'.format(args.distn), p, delimiter=',')
+    
+>>>>>>> run experiment and sim impute prep
--- sim_impute.py
+++ sim_impute.py
@@ -1,50 +1,71 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from synth_data import SynthCovariateData
 from lifelines import KaplanMeierFitter
 from ngboost.distns import LogNormal, Exponential, MultivariateNormal
 from ngboost.ngboost import NGBoost
+from ngboost.learners import default_tree_learner, default_linear_learner
+from ngboost.scores import MLE, CRPS
 from lifelines.statistics import logrank_test
+import argparse
 
-N = 5000
+N = 1000
 n_features = 5
 n_informative = 3
 surv_dist = 'lognormal'
 cens_dist = 'lognormal'
 
-tau = 2
-
 def logrank(Y1, Y2):
     results = logrank_test(durations_A=Y1, durations_B=Y2)
-                       #event_observed_A=E[~dem], event_observed_B=E[dem], alpha=.99)
 
     return results.p_value
 
 def ngb_impute(distribution, X, Y):
+    base_name_to_learner = {
+    "tree": default_tree_learner,
+    "linear": default_linear_learner,
+    }
+    
     ngb = NGBoost(Dist=distribution,
           n_estimators=200,
           learning_rate=.05,
           natural_gradient=True,
-          verbose=True,
+          verbose=False,
           minibatch_frac=1.0,
           Base=base_name_to_learner["tree"],
           Score=MLE)
     
     train = ngb.fit(X, Y)
     Y_imputed = np.copy(Y)
-    Y_imputed['Time'][Y['Event']] = np.exp(train.predict(X[Y['Event']]))
+    Y_imputed['Time'][Y['Event']] = np.exp(train.predict(X[Y['Event']]))[:len(Y_imputed['Time'][Y['Event']])]
     return Y_imputed
 
-def impute_data(distribution):
+def impute_data(distribution, tau=0, rho=0.0, obs_conf=False):
 
-    synth = SynthCovariateData(N, n_features, n_informative, surv_dist, cens_dist)
-    treat_X, treat_obsY, treat_Y, treat_C  = synth.make_linear(tau, bias_Y=0.5, bias_C=0.5, sigma_Y=1.0, sigma_C=0.8)
-    control_X, control_obsY, control_Y, control_C = synth.make_linear(0, bias_Y=0.5, bias_C=0.5, sigma_Y=1.0, sigma_C=0.8)
+    synth = SynthCovariateData(N, n_features, n_informative, 
+                               observe_confounding=obs_conf, 
+                               surv_dist='lognormal', cens_dist='lognormal')
+    treat_X, treat_obsY, treat_Y, treat_C  = synth.make_linear(tau=tau, 
+                                                               bias_Y=0.5, 
+                                                               bias_C=0.5, 
+                                                               sigma_Y=0.1, 
+                                                               sigma_C=0.1, 
+                                                               rho=rho)
+    control_X, control_obsY, control_Y, control_C = synth.make_linear(tau=0, 
+                                                                      bias_Y=0.5, 
+                                                                      bias_C=0.5, 
+                                                                      sigma_Y=0.1, 
+                                                                      sigma_C=0.1, 
+                                                                      rho=rho)
     
     treat_Y_imputed = ngb_impute(distribution, treat_X, treat_obsY)
     control_Y_imputed = ngb_impute(distribution, control_X, control_obsY)
     
     p_val = logrank(treat_Y_imputed['Time'], control_Y_imputed['Time'])
+    
+    print(p_val)
+    return p_val
     
 
 def KM(Y, observed_censoring=True):
@@ -55,4 +76,14 @@ def KM(Y, observed_censoring=True):
     else:
         kmf.fit(Y)
         
+    kmf.survival_function_.plot()
+    plt.title('Survival function of synthetic censored data');
+        
     return kmf
+
+def run_sim(N, distribution, tau, rho, obs_conf):
+    p_values = np.zeros(N)
+    for i in range(N):
+        p_values[i] = impute_data(distribution, tau=tau, rho=rho, obs_conf=obs_conf)
+        
+    return p_values
