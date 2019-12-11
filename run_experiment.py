2b9368a0450b3ed5bc748d3da902a53effa48087
fl16180 <fredlu.flac@gmail.com>
2019-12-10 16:28:38 -0800

    scripting

--- run_experiment.py
+++ run_experiment.py
@@ -1,17 +1,72 @@
+import os
 import argparse
-from sim_impute import *
+import random
+import string
+from subprocess import check_call
 
 
+RHOS = [-0.4, -0.2, 0, 0.2, 0.4]
+TAUS = [0.0, 0.05, 0.1]
+ESTS = ['lognorm', 'mvnorm']
+DISTS = ['lognormal']
+CONFOUNDING = [False]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tau', type=float, required=True)
-    parser.add_argument('--num_sim', type=int, default=10)
-    parser.add_argument('--distn', type=str, default="LogNormal")
-    parser.add_argument('--rho', type=float, default=0.0)
-    parser.add_argument('--obs_conf', type=bool, default=False)
+    parser.add_argument(
+                    '--spawn_type',
+                    type=str,
+                    choices=('stdout', 'shell', 'sbatch'),
+                    default='stdout',
+                    help='Whether to print commands to stdout (stdout), spawn\
+                          them on the current machine (shell), or spawn\
+                          them using sbatch (sbatch).')
     args = parser.parse_args()
-    
-    p = run_sim(args.num_sim, eval(args.distn), args.tau, args.rho, args.obs_conf)
-    
-    np.savetxt('{}.out'.format(args.distn), p, delimiter=',')
+    spawn_type = args.spawn_type
+
+    commands = []
+    for tau in TAUS:
+        for rho in RHOS:
+            for est in ESTS:
+                for dist in DISTS:
+                    for obs_conf in CONFOUNDING:
+                        command = 'python sim_impute.py'
+                        command += f' --tau {tau}'
+                        command += f' --rho {rho}'
+                        command += f' --est {est}'
+                        command += f' --dist {dist}'
+                        command += f' --obs_conf {obs_conf}'
+                        command += f' --num_sim 10'
+
+                        name = f'run_{tau}_{rho}_{est}_{dist}_{obs_conf}'
+                        commands.append((name, command))
+
+
+    if args.spawn_type == 'stdout':
+        # Print commands to stdout.
+        for _, command in commands:
+            print(command)
+
+    elif args.spawn_type == 'shell':
+        # Run commands sequentially on this machine.
+        commands_str = ";".join([com for _, com in commands])
+        print(commands_str)
+        check_call(commands_str, shell=True)
+
+    else:
+        # Output the commands to a sbatch script then call sbatch.
+        with open("./scripts/sbatch_template.sh") as f:
+            template = f.read()
+
+        for name, command in commands:
+            sbatch = template.replace("COMMAND", command)
+            sbatch = sbatch.replace("NAME", name)
+
+            with open("spawn_tmp.sh", 'w') as f:
+                f.write(sbatch)
+
+            print(command)
+            check_call("sbatch spawn_tmp.sh", shell=True)
+
+        os.remove("spawn_tmp.sh")
--- sim_impute.py
+++ sim_impute.py
@@ -1,89 +1,125 @@
+import argparse
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from synth_data import SynthCovariateData
 from lifelines import KaplanMeierFitter
-from ngboost.distns import LogNormal, Exponential, MultivariateNormal
 from ngboost.ngboost import NGBoost
+from ngboost.distns import LogNormal, MultivariateNormal
 from ngboost.learners import default_tree_learner, default_linear_learner
 from ngboost.scores import MLE, CRPS
 from lifelines.statistics import logrank_test
-import argparse
 
-N = 1000
-n_features = 5
-n_informative = 3
-surv_dist = 'lognormal'
-cens_dist = 'lognormal'
 
+str_to_estimator = {'lognorm': LogNormal, 'mvnorm': MultivariateNormal}
+
+N = 5000
+N_FEAT = 5
+N_INFO = 3
+BIAS_Y=0.5
+BIAS_C=0.5
+SIGMA_Y=0.1
+SIGMA_C=0.1
+LEARNER = 'linear'
+
+
+# ----- survival tools ----- #
 def logrank(Y1, Y2):
     results = logrank_test(durations_A=Y1, durations_B=Y2)
-
     return results.p_value
 
-def ngb_impute(distribution, X, Y):
+
+def KM(Y):
+    kmf = KaplanMeierFitter()
+
+    kmf.fit(Y['Time'], event_observed=Y['Event'])
+
+    kmf.survival_function_.plot()
+    plt.title('Survival function of synthetic censored data');
+    return kmf
+
+
+# ----- simulation functions ----- #
+def ngb_impute(estimator, X, Y):
     base_name_to_learner = {
     "tree": default_tree_learner,
     "linear": default_linear_learner,
     }
-    
-    ngb = NGBoost(Dist=distribution,
+
+    ngb = NGBoost(Dist=estimator,
           n_estimators=200,
           learning_rate=.05,
           natural_gradient=True,
           verbose=False,
           minibatch_frac=1.0,
-          Base=base_name_to_learner["tree"],
+          Base=base_name_to_learner[LEARNER],
           Score=MLE)
-    
+
     train = ngb.fit(X, Y)
     Y_imputed = np.copy(Y)
     Y_imputed['Time'][Y['Event']] = np.exp(train.predict(X[Y['Event']]))[:len(Y_imputed['Time'][Y['Event']])]
     return Y_imputed
 
-def impute_data(distribution, tau=0, rho=0.0, obs_conf=False):
 
-    synth = SynthCovariateData(N, n_features, n_informative, 
-                               observe_confounding=obs_conf, 
-                               surv_dist='lognormal', cens_dist='lognormal')
-    treat_X, treat_obsY, treat_Y, treat_C  = synth.make_linear(tau=tau, 
-                                                               bias_Y=0.5, 
-                                                               bias_C=0.5, 
-                                                               sigma_Y=0.1, 
-                                                               sigma_C=0.1, 
-                                                               rho=rho)
-    control_X, control_obsY, control_Y, control_C = synth.make_linear(tau=0, 
-                                                                      bias_Y=0.5, 
-                                                                      bias_C=0.5, 
-                                                                      sigma_Y=0.1, 
-                                                                      sigma_C=0.1, 
-                                                                      rho=rho)
-    
-    treat_Y_imputed = ngb_impute(distribution, treat_X, treat_obsY)
-    control_Y_imputed = ngb_impute(distribution, control_X, control_obsY)
-    
+def compute_survival_pvals(estimator, distn, tau=0, rho=0.0, obs_conf=False):
+
+    synth = SynthCovariateData(N, n_features=N_FEAT, n_informative=N_INFO,
+                               observe_confounding=obs_conf,
+                               surv_dist=distn, cens_dist=distn)
+
+    treat_X, treat_obsY, _, _  = synth.make_linear(tau=tau,
+                                                   bias_Y=BIAS_Y,
+                                                   bias_C=BIAS_C,
+                                                   sigma_Y=SIGMA_Y,
+                                                   sigma_C=SIGMA_C,
+                                                   rho=rho)
+    control_X, control_obsY, _, _ = synth.make_linear(tau=0,
+                                                      bias_Y=BIAS_Y,
+                                                      bias_C=BIAS_C,
+                                                      sigma_Y=SIGMA_Y,
+                                                      sigma_C=SIGMA_C,
+                                                      rho=rho)
+
+    treat_Y_imputed = ngb_impute(estimator, treat_X, treat_obsY)
+    control_Y_imputed = ngb_impute(estimator, control_X, control_obsY)
+
     p_val = logrank(treat_Y_imputed['Time'], control_Y_imputed['Time'])
-    
+
     print(p_val)
     return p_val
-    
 
-def KM(Y, observed_censoring=True):
-    kmf = KaplanMeierFitter()
 
-    if observed_censoring:
-        kmf.fit(Y['Time'], event_observed=Y['Event'])
-    else:
-        kmf.fit(Y)
-        
-    kmf.survival_function_.plot()
-    plt.title('Survival function of synthetic censored data');
-        
-    return kmf
+def run_sim(N_iter, estimator, distn, tau, rho, obs_conf, seed):
+    np.random.seed(seed)
 
-def run_sim(N, distribution, tau, rho, obs_conf):
-    p_values = np.zeros(N)
-    for i in range(N):
-        p_values[i] = impute_data(distribution, tau=tau, rho=rho, obs_conf=obs_conf)
-        
+    p_values = np.zeros(N_iter)
+    for i in range(N_iter):
+        p_values[i] = compute_survival_pvals(estimator,
+                                             distn,
+                                             tau=tau,
+                                             rho=rho,
+                                             obs_conf=obs_conf)
     return p_values
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tau', type=float, default=0.05)
+    parser.add_argument('--rho', type=float, default=0.0)
+    parser.add_argument('--est', type=str, choices=['lognorm', 'mvnorm'])
+    parser.add_argument('--dist', type=str, default='lognormal')
+    parser.add_argument('--obs_conf', type=bool, default=False)
+    parser.add_argument('--num_sim', type=int, default=30)
+    parser.add_argument('--seed', type=int, default=1000)
+    args = parser.parse_args()
+
+    pvals = run_sim(args.num_sim, estimators[args.est], args.dist,
+                    args.tau, args.rho, args.obs_conf, args.seed)
+
+    infos = {'tau': args.tau, 'rho': args.rho, 'est': args.est,
+             'dist': args.dist,
+             'obs_conf': args.obs_conf, 'pvals': list(pvals)}
+
+    name = f'run_{args.tau}_{args.rho}_{args.est}_{args.dist}_{args.obs_conf}'
+
+    results = pd.DataFrame(infos)
+    results.to_csv(f'./results/{name}'.csv, index=False)
